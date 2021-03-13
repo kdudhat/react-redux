989f26b2767689190badd1e548ef6fbc70602e9e
Kishan Dudhat <kishan.codexive@gmail.com>
2021-03-13 14:49:39 +0530

    it is for chrome extension(redux devTools)

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,18 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 //this middleware is used for log of state in console
 import logger from "redux-logger";
+//this enable google extension(redux devTools) in browser
+import { composeWithDevTools } from "redux-devtools-extension";
+
 import cakeReducer from "./cakes/cakeReducer";
 import iceCreamReducer from "./iceCream/iceCreamReducer";
 const rootReducer = combineReducers({
   cake: cakeReducer,
   iceCream: iceCreamReducer,
 });
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 export default store;
