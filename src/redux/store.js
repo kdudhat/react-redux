7d0f3439a84e32d7a349e44e23d230ee0e5e9985
Kishan Dudhat <kishan.codexive@gmail.com>
2021-03-13 14:33:53 +0530

    apply middleware to log state in console

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,12 @@
-import { createStore, combineReducers } from "redux";
-
+import { createStore, combineReducers, applyMiddleware } from "redux";
+//this middleware is used for log of state in console
+import logger from "redux-logger";
 import cakeReducer from "./cakes/cakeReducer";
 import iceCreamReducer from "./iceCream/iceCreamReducer";
 const rootReducer = combineReducers({
   cake: cakeReducer,
   iceCream: iceCreamReducer,
 });
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 export default store;
